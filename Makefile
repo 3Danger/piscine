9fb8e74a0f1e67c5fcaa430694f3cacd8067e997
Marc Faussurier <mfaussur@z2r1p3.le-101.fr>
2019-08-14 09:52:31 +0200

    init

--- Makefile
+++ Makefile
@@ -9,6 +9,6 @@ d604:
 main:
 	gcc  days_c/00/*/*.c days_c/01/*/*.c days_c/02/*/*.c days_c/03/*/*.c days_c/04/*/*.c days_c/05/*/*.c  main.c -std=gnu99 -Wall -Wextra -Werror && make d600 && make d601 && make d602 && make d603 && make d604
 mainall:
-	make main && ./a.out all
+	make main
 watch:
 	./watch-tests.sh
--- rush.c
+++ rush.c
@@ -1,20 +1,38 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_putchar.c                                     .::    .:/ .      .::   */
+/*   rush.c                                           .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: mfaussur <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2019/08/01 18:06:16 by mfaussur     #+#   ##    ##    #+#       */
-/*   Updated: 2019/08/14 09:47:29 by mfaussur    ###    #+. /#+    ###.fr     */
+/*   Created: 2019/08/11 13:20:02 by mfaussur     #+#   ##    ##    #+#       */
+/*   Updated: 2019/08/11 13:37:44 by mfaussur    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
 
-void	ft_putchar(char c)
+void	putstr(char * str)
 {
-	write(1, &c, 1);
+	int	i;
+
+	i = 0;
+		while(str[i])
+			write(1, &str[i++], 1);
+}
+
+int		main(int argc, char **argv)
+{
+	int		i;
+
+	while (i < argc)	
+		putstr(argv[i++]);
+	
+	int		grid[6][6];
+	int 	x;
+	int		y;
+
+	x = 0;
+	y = 0;
+
 }
