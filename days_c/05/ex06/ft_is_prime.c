1ee0794c9c2dad56542d82b82ec4a6f49f7d5cc8
Marc Faussurier <mfaussur@z1r7p6.le-101.fr>
2019-08-20 22:01:41 +0200

    update

--- days_c/05/ex06/ft_is_prime.c
+++ days_c/05/ex06/ft_is_prime.c
@@ -6,12 +6,27 @@
 /*   By: mfaussur <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/10 21:27:15 by mfaussur     #+#   ##    ##    #+#       */
-/*   Updated: 2019/08/20 01:22:19 by mfaussur    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/08/20 17:07:24 by mfaussur    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-int	ft_is_prime(int nb)
+int		ft_sqrt3(int nb)
+{
+	int		i;
+
+	if (nb <= 0)
+		return (0);
+	i = 0;
+	while (i * i <= nb && i <= 46340)
+		if (i * i == nb)
+			return (i);
+		else
+			i += 1;
+	return (0);
+}
+
+int		ft_is_prime(int nb)
 {
 	int		sqrt;
 	int		a;
@@ -24,7 +39,7 @@ int	ft_is_prime(int nb)
 		return (1);
 	if (nb == 4 || nb == 6 || nb % 2 == 0 || nb % 3 == 0)
 		return (0);
-	sqrt = ft_sqrt(nb);
+	sqrt = ft_sqrt3(nb);
 	a = 0;
 	b = 0;
 	k = 1;
--- days_c/06/ex03/ft_sort_params.c
+++ days_c/06/ex03/ft_sort_params.c
@@ -6,7 +6,7 @@
 /*   By: mfaussur <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/08/11 03:02:32 by mfaussur     #+#   ##    ##    #+#       */
-/*   Updated: 2019/08/11 06:01:02 by mfaussur    ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/08/20 17:40:25 by mfaussur    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -23,4 +23,48 @@ int		ft_strcmp(char *s1, char *s2)
 		i += 1;
 	}
 	return (s1[i] - s2[i]);
+}
+
+void	bubul(char **tab, int n)
+{
+	int 	i;
+	int		j;
+	char	*t;
+
+	i = 0;
+	while (i < n)
+	{
+		j = 0;
+		while (j < n - 1)	
+		{
+			if (strcmp(tab[i], tab[i + 1]) < 0)
+			{
+				t = tab [i + 1];
+				tab[i + 1] = tab[i];
+				tab[i] = t;
+			}
+		}
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	unsigned int	i;
+	unsigned int	y;
+	char			*toto[argc - 1];
+	char			*swp;
+
+
+	i = 1;
+	while (1 < argc)
+	{
+		toto[i - 1] = argv[i];
+		i += 1;
+	}
+	bubul(argv, argc);
+	i = 0;
+	while (i < argc)
+	{
+		printf("%s", toto[i]);
+	}
 }
